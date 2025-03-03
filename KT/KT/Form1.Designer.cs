11d28113356461a2d9e4debe6eddb191ad2a60d0
dt-kimtuyen <kttuyencathiep@gmail.com>
2025-03-03 08:51:09 +0700

    create MK

--- KT/KT/Form1.Designer.cs
+++ KT/KT/Form1.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            txtUserName = new TextBox();
+            label2 = new Label();
+            txtmatkhau = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(117, 90);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 25);
+            label1.TabIndex = 0;
+            label1.Text = "UserName";
+            // 
+            // txtUserName
+            // 
+            txtUserName.Location = new Point(276, 90);
+            txtUserName.Name = "txtUserName";
+            txtUserName.Size = new Size(150, 31);
+            txtUserName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(120, 211);
+            label2.Name = "label2";
+            label2.Size = new Size(86, 25);
+            label2.TabIndex = 2;
+            label2.Text = "Mat khau";
+            // 
+            // txtmatkhau
+            // 
+            txtmatkhau.Location = new Point(298, 208);
+            txtmatkhau.Name = "txtmatkhau";
+            txtmatkhau.Size = new Size(150, 31);
+            txtmatkhau.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtmatkhau);
+            Controls.Add(label2);
+            Controls.Add(txtUserName);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtUserName;
+        private Label label2;
+        private TextBox txtmatkhau;
     }
 }
